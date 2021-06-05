ce9f0adac33a83e702df89f398bb1950252fa244
Eric DARIEL <eric.dariel@niji.fr>
2021-06-05 20:14:05 +0200

    Add configuration

--- modules/billboard-provider-js/src/index.js
+++ modules/billboard-provider-js/src/index.js
@@ -12,23 +12,21 @@ import "billboard.js/dist/billboard.css";
 // or theme style. Find more themes from 'theme' folder
 import "billboard.js/dist/theme/insight.css"
 
-const dinamycValues = (chartAttributes) => {
+const dynamicValues = (chartAttributes) => {
   return bb.generate({
     bindto: chartAttributes.id,
     data: {
       // for ESM import usage, import 'line' module and execute it as
       // type: line(),
       type: chartAttributes.type,
-      columns: [
-          ["data1", 30, 200, 100, 400, 150, 250]
-      ]
+      columns: chartAttributes.columns
     },
     zoom: {
       // for ESM import usage, import 'zoom' module and execute it as
       // enabled: zoom()
-      enabled: true
+      enabled: chartAttributes.enabledZoom
     }
 });
 }
 
-export {dinamycValues};
+export {dynamicValues};
